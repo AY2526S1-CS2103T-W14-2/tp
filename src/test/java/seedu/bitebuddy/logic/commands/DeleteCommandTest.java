5b17d7dc828e6c6a89ea82e34f9dc74ee1882a3e
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-10-08 09:17:13 +0800

    Change access modifiers

--- src/test/java/seedu/bitebuddy/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/bitebuddy/logic/commands/DeleteCommandTest.java
@@ -25,7 +25,7 @@ import seedu.bitebuddy.model.foodplace.Foodplace;
  */
 public class DeleteCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void execute_validIndexUnfilteredList_success() {
